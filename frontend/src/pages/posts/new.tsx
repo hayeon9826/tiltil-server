d5ec5561485ffe9adf3749ba97d8510a22a7f7b5
hayeon9826 <hayeon226@likelion.org>
2022-01-12 01:39:02 +0900

    add tui editor, add react-select

--- frontend/src/pages/posts/new.tsx
+++ frontend/src/pages/posts/new.tsx
@@ -1,31 +1,57 @@
 import Header from "@components/Header";
+import dynamic from "next/dynamic";
+import * as React from "react";
+import "@toast-ui/editor/dist/toastui-editor.css";
+import { Editor as EditorType, EditorProps } from "@toast-ui/react-editor";
+import { TuiEditorWithForwardedProps } from "./TuiEditorWrapper";
+import Select from "react-select";
 
-export default function Example() {
+const options = [
+  { value: "chocolate", label: "Chocolate" },
+  { value: "strawberry", label: "Strawberry" },
+  { value: "vanilla", label: "Vanilla" },
+];
+interface EditorPropsWithHandlers extends EditorProps {
+  onChange?(value: string): void;
+}
+
+const Editor = dynamic<TuiEditorWithForwardedProps>(
+  () => import("./TuiEditorWrapper"),
+  { ssr: false }
+);
+const EditorWithForwardedRef = React.forwardRef<
+  EditorType | undefined,
+  EditorPropsWithHandlers
+>((props, ref) => (
+  <Editor {...props} forwardedRef={ref as React.MutableRefObject<EditorType>} />
+));
+
+export default function PostNew() {
   return (
     <div className="w-full">
       <Header />
-      <div className="max-w-4xl mx-auto flex flex-col md:px-8 xl:px-0 py-16">
-        <form className="space-y-8 divide-y divide-gray-200">
-          <div className="space-y-8 divide-y divide-gray-200">
+      <div className="lg:max-w-6xl mx-auto flex flex-col md:px-8 sm:px-2 xl:px-0 py-16">
+        <form className="space-y-8 divide-y">
+          <div className="space-y-8 divide-y">
             <div>
               <div>
-                <h3 className="text-lg leading-6 font-medium text-gray-900">
+                <h3 className="text-xl leading-6 font-semibold text-gray-900">
                   플래시카드 작성
                 </h3>
                 <p className="mt-1 text-sm text-gray-500">
-                  플래시 카드를 작성해주세요.
+                  문제와 답변을 작성해주세요.
                 </p>
               </div>
 
-              <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
+              <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6 pt-4 border-t">
                 <div className="sm:col-span-6">
                   <label
                     htmlFor="username"
-                    className="block text-sm font-medium text-gray-700"
+                    className="block text-base font-medium text-gray-700"
                   >
                     문제
                   </label>
-                  <div className="mt-1 flex rounded-md shadow-sm">
+                  <div className="mt-1 flex rounded-md ">
                     <input
                       type="text"
                       name="username"
@@ -39,22 +65,34 @@ export default function Example() {
                 <div className="sm:col-span-6">
                   <label
                     htmlFor="about"
-                    className="block text-sm font-medium text-gray-700"
+                    className="block text-base font-medium text-gray-700"
                   >
                     답변
                   </label>
                   <div className="mt-1">
-                    <textarea
-                      id="about"
-                      name="about"
-                      rows={16}
-                      className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border border-gray-300 rounded-md"
-                      defaultValue={""}
+                    <Editor
+                      initialValue="플래시 카드를 작성해주세요 :)"
+                      previewStyle="vertical"
+                      height="1000px"
+                      initialEditType="markdown"
+                      useCommandShortcut={true}
                     />
                   </div>
                 </div>
 
                 <div className="sm:col-span-6">
+                  <label
+                    htmlFor="about"
+                    className="block text-base font-medium text-gray-700"
+                  >
+                    카테고리
+                  </label>
+                  <div className="mt-1">
+                    <Select options={options} isMulti={true} />
+                  </div>
+                </div>
+
+                {/* <div className="sm:col-span-6">
                   <label
                     htmlFor="cover-photo"
                     className="block text-sm font-medium text-gray-700"
@@ -96,7 +134,7 @@ export default function Example() {
                       </p>
                     </div>
                   </div>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
@@ -105,13 +143,13 @@ export default function Example() {
             <div className="flex justify-end">
               <button
                 type="button"
-                className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="bg-white py-2 px-4 border border-gray-300 rounded-md border text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
                 취소
               </button>
               <button
                 type="submit"
-                className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent border text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
                 작성하기
               </button>
