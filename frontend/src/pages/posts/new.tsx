7fd645b2ee17b92b896aa0b8464363997cb18c5b
hayeon9826 <hayeon226@likelion.org>
2022-01-18 23:55:47 +0900

    edit post form

--- frontend/src/pages/posts/new.tsx
+++ frontend/src/pages/posts/new.tsx
@@ -77,7 +77,6 @@ export default function PostNew() {
   useEffect(() => {
     setValue("content", "");
     setValue("title", "");
-    // setValue("category", [1, 2, 4]);
     getCategories();
     console.log("ss");
   }, []);
@@ -245,7 +244,7 @@ export default function PostNew() {
               </button>
               <button
                 type="submit"
-                className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent border text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="ml-3 inline-flex justify-center py-2 px-4 border-transparent border text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
                 작성하기
               </button>
