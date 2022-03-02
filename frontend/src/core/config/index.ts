98487200a8bf40cdde7c5db777839e30eae90dfb
hayeon9826 <hayeon226@likelion.org>
2022-03-03 03:08:16 +0900

    edit api url

--- frontend/src/core/config/index.ts
+++ frontend/src/core/config/index.ts
@@ -1,8 +1,17 @@
-export const API_URL = "http://localhost:3000";
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://tiltil-server.herokuapp.com/'"
+    : "http://localhost:3000";
 
 export const configs = {
-  API_URL: process.env.API_URL || "http://localhost:3000",
-  SERVICE_URL: process.env.SERVICE_URL || "http://localhost:8080",
+  API_URL:
+    process.env.NODE_ENV === "production"
+      ? "https://tiltil-server.herokuapp.com/'"
+      : "http://localhost:3000",
+  SERVICE_URL:
+    process.env.NODE_ENV === "production"
+      ? "https://tiltil-server.vercel.app/"
+      : "http://localhost:8080",
   ENV: process.env.NODE_ENV || "development",
   VERSION: process.env.VERSION || "1",
 };
