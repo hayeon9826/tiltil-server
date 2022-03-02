e4b79cdaea668eb19f24fca82c3cb3d48cdfaf61
hayeon9826 <hayeon226@likelion.org>
2022-03-03 00:05:46 +0900

    add pem key path

--- backend/config/deploy/production.rb
+++ backend/config/deploy/production.rb
@@ -1,3 +1,8 @@
 server "54.180.91.136", user: "ubuntu", roles: %w[app db web]
 
 set :rails_env, :production
+
+set :rails_env, "production"
+set :stage, :production
+## SSH Remote 설정 (서버 아이디 및 pem Key 경로 설정)
+set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(/Users/apple/Desktop/programming/deploy_keys/tiltil-key.pem) }
